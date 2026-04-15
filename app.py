16c70763a0574754740f279127923ac89ebbe03f
Sami P <sammyop@gmail.com>
2026-04-15 18:46:54 +0300

    Lisätään docker kamat

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, render_template
 
 app = Flask(__name__)
@@ -32,4 +33,5 @@ def esimerkki():
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)
